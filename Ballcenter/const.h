cbc8100a4bb6a83b436eee7e1b945ab268d35af8
Simon McFarlane <simon@piston.pw>
2014-12-06 19:11:57 -0800

    Update constants

--- Ballcenter/const.h
+++ Ballcenter/const.h
@@ -1,14 +1,15 @@
 // Input settings
 #define WIDTH 1024
-#define HEIGHT 468
+#define HEIGHT 768
 
 // Window settings
 #define MAIN_WIN "Ballcenter"
 #define OUT_WIN "Output"
+#define CIRCLE_COLOR 100, 0, 0
 
 // Hough constants
-#define UPPER_EDGE 64
-#define CENTER_THRESH 20
+#define UPPER_EDGE 128
+#define CENTER_THRESH 35
 #define MIN_RADIUS 0
 #define MAX_RADIUS 0
 #define MIN_DIST HEIGHT / 8
