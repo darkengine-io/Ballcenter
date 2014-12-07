27ca2fe69bc6fb3acc7acdc97c0a851d47dc1cea
Simon McFarlane <simon@piston.pw>
2014-12-06 17:10:50 -0800

    Add debugging functionality

---
+++ Ballcenter/debug.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include "debug.h"
+
+bool dprint(std::string out){
+	std::cout << out << std::endl;
+	return false;
+}
---
+++ Ballcenter/debug.h
@@ -0,0 +1,6 @@
+// This file defines a function called dp which will only evaluate its parameters when debug is set to true
+#define debug false
+
+#define dp(out) if(debug && dprint(out));
+
+bool dprint(std::string out);
