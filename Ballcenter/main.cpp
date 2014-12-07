56d5c8a6e26b7c0e96b0ade8fac05ad78d71c84b
Simon McFarlane <simon@piston.pw>
2014-12-06 19:11:42 -0800

    Move circle detection and drawing into header file

--- Ballcenter/main.cpp
+++ Ballcenter/main.cpp
@@ -4,45 +4,28 @@
 #include <stdio.h>
 #include "const.h"
 #include "debug.h"
+#include "circles.h"
 
 using namespace cv;
 
 int main(int argc, char** argv)
 {
 	dp("Starting");
-	Mat src, src_gray;
-
+	
 	dp("Setting up capture");
-	VideoCapture cam(0);
-	cam.set(CV_CAP_PROP_FRAME_HEIGHT, 1024);
-	cam.set(CV_CAP_PROP_FRAME_WIDTH, 768);
+	bc::open(0);
+
 
 	dp("Opening windows");
 	namedWindow(MAIN_WIN, CV_WINDOW_AUTOSIZE);
+	namedWindow(OUT_WIN, CV_WINDOW_AUTOSIZE);
 	while (true){
-		cam >> src;
-		/// Convert it to gray
-		cvtColor(src, src_gray, CV_BGR2GRAY);
-
-		/// Reduce the noise so we avoid false circle detection
-		GaussianBlur(src_gray, src_gray, Size(9, 9), 2, 2);
-
-		vector<Vec3f> circles;
-
-		/// Apply the Hough Transform to find the circles
-		HoughCircles(src_gray, circles, CV_HOUGH_GRADIENT, 1, MIN_DIST, UPPER_EDGE, CENTER_THRESH, MIN_RADIUS, MAX_RADIUS);
-		dp("Circles: " + std::to_string(circles.size()));
+		bc::get_frame();
+		
+		bc::draw_circles(bc::find_circles());
 		/// Draw the circles detected
-		for (size_t i = 0; i < circles.size(); i++)
-		{
-			Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-			int radius = cvRound(circles[i][2]);
-			// circle center
-			circle(src, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-			// circle outline
-			circle(src, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-		}
-		imshow(MAIN_WIN, src);
+
+		//imshow(MAIN_WIN, src);
 		if (waitKey(1) >= 0) break;
 	}
 	return 0;
